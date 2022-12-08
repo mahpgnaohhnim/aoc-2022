6fbe58c022e22d1f69f70bf5edf2b8b04015a89d
Pham Minh Hoang Henry <minhhoangpham@live.de>
2022-12-08 15:23:38 +0100

    feature: add kotlin day 05 part 2

--- aoc-kotlin/src/main/kotlin/day05/Day05.kt
+++ aoc-kotlin/src/main/kotlin/day05/Day05.kt
@@ -3,49 +3,62 @@ package day05
 import java.io.File
 
 class Day05 {
-    companion object{
-        fun getResultText(inputText: String): String{
+    companion object {
+        fun getResultText(inputText: String, part2: Boolean = false): String {
             val parts = inputText.split("\n\n")
             val data = parseData(parts[0])
-            executeCommands(parts[1], data)
-            return data.map { it.last() }.joinToString("")
+            executeCommands(parts[1], data, part2)
+            return data.map {
+                when {
+                    it.isEmpty() -> ""
+                    else -> it.last()
+                }
+            }.joinToString("")
         }
 
         fun parseData(input: String): List<MutableList<String>> {
             val lines = input.lines().asReversed().toMutableList()
             val data = getEmptyDataByBaseLine(lines[0])
             lines.removeFirst()
-            lines.map { parseDateLine(it,data) }
+            lines.map { parseDateLine(it, data) }
             return data
         }
 
         fun parseDateLine(line: String, data: List<MutableList<String>>) {
             val pattern = "(\\[)|(])".toRegex()
-            val chunks = line.chunked(4).map { it.replace(pattern,"").trim() }
+            val chunks = line.chunked(4).map { it.replace(pattern, "").trim() }
             chunks.forEachIndexed { index, chunk ->
                 val dataGroup = data[index]
-                if(chunk.isNotBlank()){
+                if (chunk.isNotBlank()) {
                     dataGroup.add(chunk)
                 }
             }
         }
 
-        fun getEmptyDataByBaseLine(baseLine: String): List<MutableList<String>>{
+        fun getEmptyDataByBaseLine(baseLine: String): List<MutableList<String>> {
             val chunks = baseLine.chunked(4)
             return chunks.map { mutableListOf() }
         }
 
-        fun executeCommands(commandsPart: String, data: List<MutableList<String>>){
+        fun executeCommands(commandsPart: String, data: List<MutableList<String>>, part2: Boolean) {
             commandsPart.lines().forEach { line ->
                 val match = Regex("move (\\d+) from (\\d+) to (\\d+)").find(line)
-                if(match != null){
+                if (match != null) {
                     val (count, fromGroupId, targetGroupId) = match.destructured
 
-                    val fromGroup = data[fromGroupId.toInt()-1]
-                    val targetGroup = data[targetGroupId.toInt()-1]
-                    repeat(count.toInt()){
-                        targetGroup.add(fromGroup.last())
-                        fromGroup.removeLast()
+                    val fromGroup = data[fromGroupId.toInt() - 1]
+                    val targetGroup = data[targetGroupId.toInt() - 1]
+                    if (part2) {
+                        val groupData = fromGroup.takeLast(count.toInt())
+                        targetGroup.addAll(groupData)
+                        repeat(count.toInt()) {
+                            fromGroup.removeLast()
+                        }
+                    } else {
+                        repeat(count.toInt()) {
+                            targetGroup.add(fromGroup.last())
+                            fromGroup.removeLast()
+                        }
                     }
                 }
             }
@@ -53,12 +66,18 @@ class Day05 {
     }
 }
 
-fun main(){
+fun main() {
     val inputFile = File(Day05::class.java.getResource("input.txt")?.path.orEmpty())
     val resultPart1 = Day05.getResultText(inputFile.readText())
+    val resultPart2 = Day05.getResultText(inputFile.readText(), true)
 
-    println("""
+    println(
+            """
         resultPart1:
         $resultPart1
-    """.trimIndent())
+        
+        resultPart2:
+        $resultPart2
+    """.trimIndent()
+    )
 }
--- aoc-kotlin/src/test/kotlin/day05/Day05Test.kt
+++ aoc-kotlin/src/test/kotlin/day05/Day05Test.kt
@@ -1,15 +1,18 @@
 package day05
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.File
 
-internal class Day05Test{
+internal class Day05Test {
     var inputFile = File(this::class.java.getResource("test1.txt")?.path.orEmpty())
 
     @Test
     fun getResultText() {
         val result = Day05.getResultText(inputFile.readText())
         assertEquals("CMZ", result)
+
+        val resultPart2 = Day05.getResultText(inputFile.readText(), true)
+        assertEquals("MCD", resultPart2)
     }
 }
